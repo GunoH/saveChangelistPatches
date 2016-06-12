6cb78077437d03ecc32fda0426b60d90c3c8644c
GunoH <git@guno.nl>
2016-06-12 21:34:14 +0200

    Cleanup

--- src/main/java/com/benrose/plugins/saveChangeListsToPatches/SaveChangeListsToPatchesAction.java
+++ src/main/java/com/benrose/plugins/saveChangeListsToPatches/SaveChangeListsToPatchesAction.java
@@ -1,29 +1,14 @@
-/*    */ package com.benrose.plugins.saveChangeListsToPatches;
-/*    */ 
-/*    */ import com.intellij.openapi.actionSystem.AnAction;
-/*    */ import com.intellij.openapi.actionSystem.AnActionEvent;
-/*    */ import com.intellij.openapi.actionSystem.DataContext;
-/*    */ import com.intellij.openapi.project.Project;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class SaveChangeListsToPatchesAction
-/*    */   extends AnAction
-/*    */ {
-/*    */   public void actionPerformed(AnActionEvent e)
-/*    */   {
-/* 19 */     Project project = (Project)e.getDataContext().getData("project");
-/*    */     
-/* 21 */     ((SaveChangeListsToPatchesApplicationComponent)project.getComponent("SaveChangeListsToPatchesApplicationComponent")).savePatches();
-/*    */   }
-/*    */ }
+package com.benrose.plugins.saveChangeListsToPatches;
 
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
 
-/* Location:              C:\Users\brose_wndz\Downloads\plugin\plugin\lib\plugin.jar!\com\benrose\plugins\saveChangeListsToPatches\SaveChangeListsToPatchesAction.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       0.7.1
- */
+public class SaveChangeListsToPatchesAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(CommonDataKeys.PROJECT);
+        ((SaveChangeListsToPatchesApplicationComponent)project
+                .getComponent("SaveChangeListsToPatchesApplicationComponent")).savePatches();
+    }
+}
