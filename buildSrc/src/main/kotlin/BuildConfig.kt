eb185f684121206c4c5b283bfb0f62b4749e42ec
GunoH <git@guno.nl>
2018-05-13 09:49:48 +0530

    v1.5.0

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -4,5 +4,5 @@ object BuildConfig {
     const val kotlinVersion = "1.2.30"
     const val gradleIntellijPluginVersion = "0.3.1"
     const val intellijVersion = "IC-2016.1"
-    const val pluginVersion = "1.5.0pre"
+    const val pluginVersion = "1.5.0"
 }
