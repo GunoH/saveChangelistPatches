c8ec2f9a673f1c0d690e2b3f64c53e4296521f2c
GunoH <git@guno.nl>
2019-02-24 04:57:23 +0100

    Upgrade Kotlin runtime to 1.3.21.

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused")
 
 object BuildConfig {
-    const val kotlinVersion = "1.2.41"
+    const val kotlinVersion = "1.3.21"
     const val gradleIntellijPluginVersion = "0.3.1"
     const val intellijVersion = "IC-2016.1"
     const val pluginVersion = "1.5.0"
